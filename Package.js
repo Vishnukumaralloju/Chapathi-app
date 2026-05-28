6092b20a32741b7cbd7b38e22d2643695b8ebd54
Vishnukumaralloju <vishnu.alloju@gmail.com>
2026-05-28 08:10:44 +0530

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,13 @@
+{
+  "name": "chapathi-app",
+  "version": "1.0.0",
+  "main": "server.js",
+  "dependencies": {
+    "express": "^4.19.2",
+    "nedb-revived": "^1.1.2"
+  },
+  "scripts": {
+    "start": "node server.js"
+  }
+}
+
